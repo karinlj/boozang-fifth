596a13191aa0fddfcfb1ddfe9dd3e0becd9f82c7
Karin Ljunggren <kaljunggren@gmail.com>
2020-08-08 22:59:09 +0200

    videos more space

--- parts/video-list.php
+++ parts/video-list.php
@@ -2,12 +2,8 @@
   <div class="container">
 
     <div class="row">
-      <!-- <div class="col-lg-10 offset-lg-1"> -->
-      <div class="col-12">
 
-        <div class="row">
-
-          <?php
+      <?php
 $post_type = 'videos';
 
 // Get all the taxonomies for this post type
@@ -18,9 +14,9 @@ foreach ($taxonomies as $taxonomy) {
     // Gets every "category" (term) in this taxonomy to get the respective posts
     $terms = get_terms($taxonomy);?>
 
-          <?php foreach ($terms as $term) {?>
+      <?php foreach ($terms as $term) {?>
 
-          <?php $posts = new WP_Query("taxonomy=$taxonomy&term=$term->slug&posts_per_page=-1");
+      <?php $posts = new WP_Query("taxonomy=$taxonomy&term=$term->slug&posts_per_page=-1");
 
         if ($posts->have_posts()) {
 
@@ -28,52 +24,52 @@ foreach ($taxonomies as $taxonomy) {
 
                 $posts->the_post();?>
 
-          <div class="col-sm-6 col-lg-4">
+      <div class="col-sm-6 col-lg-4">
 
 
-            <div class="video_container">
+        <div class="video_container">
 
-              <?php $video_url = get_field('url');
+          <?php $video_url = get_field('url');
                 $style = '';
                 $bg_img = get_field('background_image');
                 ?>
-              <div class="embed_container" <?php echo $style; ?>>
-                <!-- url -->
-                <?php if ($video_url) {?>
-                <a class="video_link" href="<?php echo $video_url; ?>">
-                </a>
-                <?php }?>
+          <div class="embed_container" <?php echo $style; ?>>
+            <!-- url -->
+            <?php if ($video_url) {?>
+            <a class="video_link" href="<?php echo $video_url; ?>">
+            </a>
+            <?php }?>
 
-                <div class="img_container">
+            <div class="img_container">
 
-                  <?php $image = get_field('image');
+              <?php $image = get_field('image');
                 $size = 'full'; // (thumbnail, medium, large, full or custom size)
                 if ($image) {
                     echo wp_get_attachment_image($image, $size);
                 }?>
 
-                  <h3 class="title_on_img"><?php the_field('image_title');?></h3>
-                </div>
-              </div>
-
-              <div class="content_container">
-
-                <?php echo '<span class="cat_badge">' . $term->name . '</span>'; ?>
-                <div class="content_container_inner">
-
-                  <h6 class="title"><?php the_title();?></h6>
-                  <p class="description"><?php the_field('description');?></p>
-                </div>
-              </div>
+              <h3 class="title_on_img"><?php the_field('image_title');?></h3>
             </div>
           </div>
-          <?php
+
+          <div class="content_container">
+
+            <?php echo '<span class="cat_badge">' . $term->name . '</span>'; ?>
+            <div class="content_container_inner">
+
+              <h6 class="title"><?php the_title();?></h6>
+              <p class="description"><?php the_field('description');?></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php
 }
         }
     }
 }?>
-        </div>
-      </div>
     </div>
+  </div>
+  </div>
   </div>
 </section>
