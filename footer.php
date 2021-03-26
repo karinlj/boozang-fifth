f34401b90e25f5966695c6e3176e0ed7b1ab98fc
Karin Ljunggren <kaljunggren@gmail.com>
2021-03-26 22:14:05 +0100

    new dark mode and content think

--- footer.php
+++ footer.php
@@ -12,48 +12,76 @@ if (function_exists('acf_add_options_page')) {
             if ($bg_image) {
                 $style = 'style="background:url(\'' . wp_get_attachment_url($bg_image, 'full') . '\') no-repeat center; background-size: cover"';
                 $overlay_color = get_sub_field('overlay_color');
-            }?>
+            } ?>
 
 
-    <section class="signup_section <?php echo $color; ?> <?php echo $overlay_color; ?> section_spacing_top_small"
-      <?php echo $style; ?>>
+    <section class="signup_section <?php echo $color; ?> <?php echo $overlay_color; ?> section_spacing_top_medium"
+        <?php echo $style; ?>>
 
-      <div class="container">
-        <div class="row">
-         
-              <div class="col-md-6">
-                <div class="text_section">
-                  <h2><?php the_sub_field('heading');?></h2>
-                  <p> <?php the_sub_field('text');?></p>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="heading">
+                        <h3 class=""><?php the_sub_field('heading'); ?></h3>
+                        <p>Cats are cute kitty for slap owner's face at 5am until human fills food dish and steal mom's
+                            crouton while she is in the bathroom, so sleep over your phone</p>
+                    </div>
                 </div>
-              </div>
-              <div class="col-md-6">
-                <div class="btn_container">
-                  <?php
+            </div>
+
+            <div class="row">
+                <div class="col-md-8 offset-2">
+                    <div class="row justify-content-between">
+                        <div class="col-md-6">
+                            <div class="signup_button">
+                                <div class="text_section">
+                                    <div>
+                                        <p> <?php the_sub_field('text'); ?></p>
+                                    </div>
+                                </div>
+                                <div class="btn_container">
+                                    <?php
                   $signup_button = get_sub_field('signup_button');
-
             if ($signup_button) {?>
+                                    <a class="btn signup_btn" href="<?php echo $signup_button['url']; ?>"
+                                        target="<?php echo $signup_button['target']; ?>"><?php echo $signup_button['title']; ?>
+                                    </a>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
 
-                  <a class="btn signup_btn" href="<?php echo $signup_button['url']; ?>"
-                    target="<?php echo $signup_button['target']; ?>"><?php echo $signup_button['title']; ?>
-                  </a>
+                        <div class="col-md-6">
+                            <div class="demo_button">
+                                <div class="text_section">
+                                    <div>
+                                        <p> <?php the_sub_field('text'); ?></p>
+                                    </div>
+                                </div>
+                                <div class="btn_container">
+                                    <?php
+                  $signup_button = get_sub_field('signup_button');
+            if ($signup_button) {?>
+                                    <a class="btn signup_btn" href="<?php echo $signup_button['url']; ?>"
+                                        target="<?php echo $signup_button['target']; ?>"><?php echo $signup_button['title']; ?>
+                                    </a>
+                                    <?php } ?>
+                                </div>
+                            </div>
 
-                  <?php }?>
-
+                        </div>
+                    </div>
                 </div>
-              </div>
-         
-        </div>
-      </div>
-    </section> 
+            </div>
+    </section>
     <?php
-}
+        }
     }
 }
 ?>
 
-<!-- boxes - no img -->
-<!-- <section class="boxes">
+    <!-- boxes - no img -->
+    <!-- <section class="boxes">
 	<div class="boxes-inner">
 		<div class="box" style="background: url(&quot;/static/media/leaves_small.770460e1.jpg&quot;);">
 			<div class="text-part-outer">
@@ -95,109 +123,111 @@ if (function_exists('acf_add_options_page')) {
 
 
     <footer id="footer" class="footer-main section_spacing_top_small">
-      <div class="container">
-        <div class="row align-items-start justify-content-between">
-          <div class="col-lg-3">
-            <div class="logo">
-              <a href="<?php echo home_url() ?>" class="logo">
+        <div class="container">
+            <div class="row align-items-start justify-content-between">
+                <div class="col-lg-3">
+                    <div class="logo">
+                        <a href="<?php echo home_url() ?>" class="logo">
 
-                <img class="logo-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/boozang_logo_reverse.png"
-                  srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/boozang_logo_reverse@2x.png 325w"
-                  width="208" height="51" alt="boozang-logo" />
-              </a>
-            </div>
-            <div class="social-icons">
-              <?php
+                            <img class="logo-img"
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/boozang_logo_reverse.png"
+                                srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/boozang_logo_reverse@2x.png 325w"
+                                width="208" height="51" alt="boozang-logo" />
+                        </a>
+                    </div>
+                    <div class="social-icons">
+                        <?php
 if (function_exists('acf_add_options_page')) {
     ?>
-              <ul class="social">
+                        <ul class="social">
 
-                <?php
-if (have_rows('social_icons', 'option')) { // check if repeater field has rows ?>
-                <?php
+                            <?php
+if (have_rows('social_icons', 'option')) { // check if repeater field has rows?>
+                            <?php
 while (have_rows('social_icons', 'option')) {
         the_row();
 
         $social_url = get_sub_field('social_url');
-        $social_site = get_sub_field('social_site');?>
+        $social_site = get_sub_field('social_site'); ?>
 
-                <li class="social-item">
-                  <a href="<?php echo $social_url; ?>"><i class="fab fa-<?php echo $social_site; ?>"></i></a>
-                </li>
+                            <li class="social-item">
+                                <a href="<?php echo $social_url; ?>"><i
+                                        class="fab fa-<?php echo $social_site; ?>"></i></a>
+                            </li>
 
-                <?php
-}?>
-                <?php
+                            <?php
+    }?>
+                            <?php
 }
-    $link = get_field('mail_link', 'option');?>
+    $link = get_field('mail_link', 'option'); ?>
 
-                <li class="social-item">
-                  <a href="mailto:<?php echo $link; ?>" target="_top"><i class="fas fa-envelope"
-                      aria-hidden="true"></i></a>
-                </li>
-              </ul>
-              <?php
+                            <li class="social-item">
+                                <a href="mailto:<?php echo $link; ?>" target="_top"><i class="fas fa-envelope"
+                                        aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                        <?php
 }
 ?>
-            </div>
-          </div>
+                    </div>
+                </div>
 
-          <div class="col-lg-9 col-xl-8">
-            <div class="row align-items-start links">
-              <?php
+                <div class="col-lg-9 col-xl-8">
+                    <div class="row align-items-start links">
+                        <?php
 if (function_exists('acf_add_options_page')) {
     if (have_rows('footer_links', 'option')) { // check if repeater field has rows
 
         while (have_rows('footer_links', 'option')) { // loop through the rows of data
-            the_row();?>
-              <div class="col-6 col-md-3">
-                <h5 class="footer-links-heading"><?php the_sub_field('heading');?> </h5>
+            the_row(); ?>
+                        <div class="col-6 col-md-3">
+                            <h5 class="footer-links-heading"><?php the_sub_field('heading'); ?> </h5>
 
-                <?php
+                            <?php
 if (have_rows('links')) { // check if repeater field has rows
                 ?>
-                <ul class="footer-links">
-                  <?php
+                            <ul class="footer-links">
+                                <?php
 while (have_rows('links')) { // loop through the rows of data
-                    the_row();?>
+                    the_row(); ?>
 
-                  <?php
-$link = get_sub_field('link');?>
+                                <?php
+$link = get_sub_field('link'); ?>
 
-                  <li class="link-item">
-                    <a href="<?php echo $link['url']; ?>"
-                      target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
-                  </li>
-                  <?php
-}?>
+                                <li class="link-item">
+                                    <a href="<?php echo $link['url']; ?>"
+                                        target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+                                </li>
+                                <?php
+                }?>
 
-                </ul>
-                <?php
-}?>
-              </div>
-              <?php
-}?>
-              <?php
-}
+                            </ul>
+                            <?php
+} ?>
+                        </div>
+                        <?php
+        } ?>
+                        <?php
+    }
 }
 ?>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="footer-copy">
-        <div class="container">
-          <div class="row align-items-start">
-            <div class="col">
-              <div class="copy">
-                <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name');?> INC. ALL RIGHTS RESERVED. Theme by <a
-                    href="http://karinljunggren.com/" target="_blank">Karin</a>
-                </p>
-              </div>
+        <div class="footer-copy">
+            <div class="container">
+                <div class="row align-items-start">
+                    <div class="col">
+                        <div class="copy">
+                            <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name');?> INC. ALL RIGHTS RESERVED.
+                                Theme by <a href="http://karinljunggren.com/" target="_blank">Karin</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </footer>
 
     <?php wp_footer();?>
--- templates/pillars.php
+++ templates/pillars.php
@@ -2,35 +2,29 @@
 $color = get_sub_field('color_theme');
 ?>
 
-<div class="container">
-    <div class="row">
-       <div class="col">
 
-        <section class="pillars_section section_spacing_top_medium">
-          <div class="container">
-            <div class="row align-items-start">
-              <?php
+
+<section class="pillars_section section_spacing_top_small">
+    <div class="container">
+        <div class="row align-items-start">
+            <?php
         // check if the repeater field has rows
         if (have_rows('pillars_content')) {
 
             while (have_rows('pillars_content')) {
                 the_row();
                 ?>
-              <div class="col-lg">
+            <div class="col-lg">
                 <div class="pillars-item">
-                  <h6 class="top-heading <?php echo $color; ?>"><?php the_sub_field('top_heading');?></h6>
-                  <h5 class="heading"><?php the_sub_field('heading');?></h5>
-                  <p class="text"><?php the_sub_field('text');?></p>
+                    <h6 class="top-heading <?php echo $color; ?>"><?php the_sub_field('top_heading');?></h6>
+                    <h5 class="heading"><?php the_sub_field('heading');?></h5>
+                    <p class="text"><?php the_sub_field('text');?></p>
                 </div>
-              </div>
-              <?php
+            </div>
+            <?php
         }
         }
         ?>
-            </div>
-          </div>
-        </section>
-</div>
-</div>
-</div>
-  
+        </div>
+    </div>
+</section>
