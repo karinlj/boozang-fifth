fe13cc0e3f19293dd932fb892554600f722aa030
Karin Ljunggren <kaljunggren@gmail.com>
2021-03-27 12:23:01 +0100

    new feature slider

--- js/custom_script.js
+++ js/custom_script.js
@@ -60,21 +60,21 @@ jQuery(document).ready(function ($) {
   });
 
   //feature slider
-  $("li.feat-menu-item.active").removeClass("active"); //remove active class on li
-  $("img.feat-img.active").removeClass("active"); //remove active class on li
-  $("li.feat-menu-item.active").addClass("inactive"); //remove active class on li
-  $("img.feat-img.active").addClass("inactive"); //remove active class on li
-  $(".comes-first li.feat-menu-item:first").addClass("active"); //set active class on first li
-  $(".comes-last img.feat-img").addClass("inactive");
-  $(".comes-last img.feat-img:first").addClass("active");
-  $(".comes-first img.feat-img").addClass("inactive");
-  $(".comes-first img.feat-img:first").addClass("active");
-  $(".comes-last li.feat-menu-item:first").addClass("active"); //set active class on first li
+  $("li.features_menu_item.active").removeClass("active"); //remove active class on li
+  $("img.feat_img.active").removeClass("active"); //remove active class on li
+  $("li.features_menu_item.active").addClass("inactive"); //remove active class on li
+  $("img.feat_img.active").addClass("inactive"); //remove active class on li
+  $(".comes-first li.features_menu_item:first").addClass("active"); //set active class on first li
+  $(".comes-last img.feat_img").addClass("inactive");
+  $(".comes-last img.feat_img:first").addClass("active");
+  $(".comes-first img.feat_img").addClass("inactive");
+  $(".comes-first img.feat_img:first").addClass("active");
+  $(".comes-last li.features_menu_item:first").addClass("active"); //set active class on first li
 
-  $("li.feat-menu-item").click(function () {
+  $("li.features_menu_item").click(function () {
     //click on span tag
 
-    $("li.feat-menu-item.active").removeClass("active"); //remove active class on li
+    $("li.features_menu_item.active").removeClass("active"); //remove active class on li
     $(this).addClass("active"); // set active class on li to make it expanded
 
     var itemId = $(this).attr("id"); // get span id
@@ -84,9 +84,9 @@ jQuery(document).ready(function ($) {
 
     $(this)
       .closest(".container")
-      .find("img.feat-img.active")
+      .find("img.feat_img.active")
       .removeClass("active"); //remove active class on image
-    $(this).closest(".container").find("img.feat-img").addClass("inactive"); //remove active class on image
+    $(this).closest(".container").find("img.feat_img").addClass("inactive"); //remove active class on image
 
     $("#" + imgId).addClass("active"); // set img active class on image to set opacity=1 and z-index=2
     $("#" + imgId).removeClass("inactive"); // set img active class on image to set opacity=1 and z-index=2
--- templates/feature_slider.php
+++ templates/feature_slider.php
@@ -3,227 +3,85 @@
 
 ?>
 <section class="two-columns-section feature_slider section_spacing_top_medium">
-<h2><?php the_sub_field('heading'); ?></h2>
-  <div class="container">
-    <div class="row justify-content-between">
-      <?php
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <!-- <h2><?php the_sub_field('heading'); ?></h2> -->
+                <?php
 $img_order = '';
 $text_order = '';
 $suffix = '';
-if (get_sub_field('image_position'))
-{ //radio btn field
+if (get_sub_field('image_position')) { //radio btn field
     $img_order = 'comes-' . get_sub_field('image_position'); //first or last
-    if ($img_order == 'comes-last')
-    {
+    if ($img_order == 'comes-last') {
         $suffix = 'x';
         $text_order = 'comes-first';
-    }
-    else
-    {
+    } else {
         $suffix = 'y';
         $text_order = 'comes-last';
     }
 } ?>
-      <div class="col-lg-4 <?php echo $text_order; ?>">
-        <div class="feat-list">
-          <!-- <h3><?php the_sub_field('heading'); ?></h3> -->
-          <p class="feature-description-mobile"><?php the_sub_field('feature_description_mobile'); ?></p>
-          <ul id="features-menu">
+                <!-- images -->
+                <div class="<?php echo $img_order; ?>">
+                    <div class="feat_slider_wrapper">
 
-            <?php
-//Loopa kolumner
-if (have_rows('feature_slider'))
-{
-    $j = 0; //räknare = 0
-    while (have_rows('feature_slider'))
-    {
-        the_row();
-        $j++; //räknare räknas upp varje varv
-        
-?>
-            <!--I have <?php echo $j; ?>-->
-
-            <?php
-        if (get_sub_field('heading_link') || get_sub_field('feature_description'))
-        { ?>
-
-            <li id="<?php echo $j ?><?php echo $suffix ?>" class="feat-menu-item inactive">
-
-              <span id="<?php echo $j ?><?php echo $suffix ?>"><?php the_sub_field('heading_link'); ?></span>
-              <p class="text"><?php the_sub_field('feature_description'); ?></p>
-            </li>
-
-            <?php
-        }
-    }
-}
-?>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-lg-8 <?php echo $img_order; ?>">
-        <div class="feat-slider-wrapper">
-
-          <?php
+                        <?php
 $imgsuffix = $suffix . "_img"; //img suffix
 //Loopa kolumner
-if (have_rows('feature_slider'))
-{
+if (have_rows('feature_slider')) {
     $i = 0; //räknare = 0
-    while (have_rows('feature_slider'))
-    {
+    while (have_rows('feature_slider')) {
         the_row();
 
-        $i++; //räknare räknas upp varje varv
-        
-?>
-          <!-- I have <?php echo $i; ?>-->
-          <?php
+        $i++; //räknare räknas upp varje varv?>
+                        <!-- I have <?php echo $i; ?>-->
+                        <?php
         $image = get_sub_field('img');
 
         //echo $image;
-        if (!empty($image))
-        { ?>
+        if (!empty($image)) { ?>
 
-          <img id="<?php echo $i ?><?php echo $imgsuffix ?>" class="feat-img" src="<?php echo $image['url']; ?>"
-            alt="<?php echo $image['alt']; ?>" />
-
-          <?php
+                        <img id="<?php echo $i ?><?php echo $imgsuffix ?>" class="feat_img"
+                            src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php
         }
     }
 }
 ?>
-        </div>
-      </div>
-    </div>
-  <div class="row">
-      <div class="col-lg-8">
-        <!-- <h6 class="preamble text-center"><?php the_sub_field('link_preamble'); ?></h6> -->
+                    </div>
+                </div>
 
-        <!-- <?php
-$link_url = get_sub_field('link_url');
-$link_text = get_sub_field('link_text'); ?>
+                <!-- btns -->
+                <div class="<?php echo $text_order; ?>">
+                    <div class="feat_list">
+                        <!-- <h3><?php the_sub_field('heading'); ?></h3> -->
+                        <!-- <p class="feature-description-mobile"><?php the_sub_field('feature_description_mobile'); ?></p> -->
+                        <ul id="features_menu">
+                            <?php
+//Loopa kolumner
+if (have_rows('feature_slider')) {
+    $j = 0; //räknare = 0
+    while (have_rows('feature_slider')) {
+        the_row();
+        $j++; //räknare räknas upp varje varv?>
+                            <!--I have <?php echo $j; ?>-->
 
-        <div class="link_wrapper text-center">
-          <a class="arrow_link" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
-        </div> -->
-      </div>
-    </div> 
-
-  </div>
-
-
-     <!-- <section class="feature_link_item_section section_spacing_top_small">
-  <h2 class="preamble text-center"><?php the_sub_field('link_preamble'); ?></h2>
-
-  <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-lg-10">
-      
-    <div class="row justify-content-between">
-    <div class="col-lg-3">
-      <a href="https://localhost/Boozang/feature-overview/" class="feature_link_item">
-      <div class="icon">
-    <i class="fas fa-database"></i>
-    </div>      <h6>Feature overview</h6>
-      <p>Go from simple record-replay tests to powerful data-driven tests.</p>
-</a>
-    </div>
-    <div class="col-lg-3">
-    <a href="https://localhost/Boozang/feature-overview/" class="feature_link_item">
-    <div class="icon">
-    <i class="fas fa-database"></i>
-    </div>
-      <h6>BDD/Cucumber support</h6>
-      <p>Offload the development team by using business developers to write the tests.</p>
-      </a>
-    </div>
-    <div class="col-lg-3">
-    <a href="https://localhost/Boozang/feature-overview/" class="feature_link_item">
-    <div class="icon">
-    <i class="fas fa-database"></i>
-    </div>      <h6>Data Handling</h6>
-      <p>Use data to create truly re-usable tests and use external data to drive your tests.</p>
-      </a>
-    </div>
-    <div class="col-lg-3">
-    <a href="https://localhost/Boozang/feature-overview/" class="feature_link_item">
-    <div class="icon">
-    <i class="fas fa-database"></i>
-    </div>      <h6>Feature overview</h6>
-      <p>Go from simple record-replay tests to powerful data-driven tests.</p>
-      </a>
-    </div>
-    </div>
-</div>
-</div>
-  </section>  -->
-
-   <!-- <div class="banner_link">Go to The Lab</div> -->
- <!-- <div class="container">
-    <section class="img_text_section section_spacing_top_small">
-          <div class="column_row"> 
-            <div class="part text">
-              <h2>theLab <span><i class="fas fa-graduation-cap"></i></span></h2>
-              <p>Much more than a simple record-and-replay tool, Boozang provides IDE-style visual interface that allow you to create and maintain tests for complex projects.</p>
-              <p>Work on several tests at once using multiple tabs, just like any desktop IDE. Work on several tests at once using multiple tabs, just like any desktop IDE.</p>
+                            <?php
+        if (get_sub_field('heading_link') || get_sub_field('feature_description')) { ?>
+                            <li id="<?php echo $j ?><?php echo $suffix ?>" class="features_menu_item inactive">
+                                <span
+                                    id="<?php echo $j ?><?php echo $suffix ?>"><?php the_sub_field('heading_link'); ?></span>
+                                <!-- <p class="text"><?php the_sub_field('feature_description'); ?></p> -->
+                            </li>
+                            <?php
+        }
+    }
+}
+?>
+                        </ul>
+                    </div>
+                </div>
             </div>
-              <div class="part image">
-              <a href="http://thelab.boozang.com/" class="img_link">
-              <img src="https://localhost/Boozang/wp-content/uploads/2021/03/thelabTables.jpg" target="_blank" alt="theLab"/>
-                </a>
         </div>
-      </div>
-      </div>  -->
-
-
-
-
-
-
-
-<!-- <section class="boxes">
-	<div class="boxes-inner">
-		<div class="box" style="background: url(&quot;/static/media/leaves_small.770460e1.jpg&quot;);">
-			<div class="text-part-outer">
-				<div class="text-part-inner"><i class="fas fa-laptop boxes-icon"></i>
-					<h3 class="heading"> Web Technologies &amp; other</h3>
-					<div class="text">
-						<p><b>Git</b>, NPM, Webpack, Azure DevOps, Firebase.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="box" style="background: url(&quot;/static/media/leaves_small.770460e1.jpg&quot;);">
-			<div class="text-part-outer">
-				<div class="text-part-inner"><i class="fas fa-graduation-cap boxes-icon"></i>
-					<h3 class="heading"> theLab</h3>
-					<div class="text">
-						<p>Try to jump onto window and fall while scratching at wall sit in window and stare oooh, a bird, yum. Cat cat moo moo lick ears lick paws run at 3am human is washing you why halp oh the horror.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="box" style="background: url(&quot;/static/media/leaves_small.770460e1.jpg&quot;);">
-			<div class="text-part-outer">
-				<div class="text-part-inner"><i class="fas fa-wordpress boxes-icon"></i><i class="fab fa-wordpress boxes-icon"></i>
-					<h3 class="heading"> CMS/Wordpress/Contentful</h3>
-					<div class="text">
-						<p><b>Custom WP themes</b> - dynamic and lightweight, best bulit with ACF. Woocommerce themes for webshops. Also developing with headless CMS <b>Sanity.io </b>or <b>Contentful</b>.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section> -->
-
-
-
-
-
-
-    </section>
-  </div>
-
+    </div>
 </section>
