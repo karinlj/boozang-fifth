ea605a9ff1fdbaa1277d0830223e1fcb506eced0
Karin Ljunggren <kaljunggren@gmail.com>
2022-05-26 23:26:26 +0200

    accessibility: big_image and feature_blocks section

--- templates/big_image_center.php
+++ templates/big_image_center.php
@@ -1,18 +1,18 @@
 <?php
 // centered big_image_center for Front Page Block
 
-if (get_row_layout() == 'big_image_center') {?>
+if (get_row_layout() == 'big_image_center') { ?>
 
 <?php
-$color = get_sub_field('background_color');
-$heading = get_sub_field('heading');
-$preamble = get_sub_field('preamble');
+    $color = get_sub_field('background_color');
+    $heading = get_sub_field('heading');
+    $preamble = get_sub_field('preamble');
 
-$link_url = get_sub_field('link_url');
+    $link_url = get_sub_field('link_url');
 
-$img_id = get_sub_field('img');
-$image = wp_get_attachment_image_src($img_id, 'full');
-$alt_text = get_post_meta($img_id, '_wp_attachment_image_alt', true);?>
+    $img_id = get_sub_field('img');
+    $image = wp_get_attachment_image_src($img_id, 'full');
+    $alt_text = get_post_meta($img_id, '_wp_attachment_image_alt', true); ?>
 
 <section class="big_image_center <?php echo $color; ?> section_spacing_top_small">
     <div class="container">
@@ -21,7 +21,8 @@ $alt_text = get_post_meta($img_id, '_wp_attachment_image_alt', true);?>
                 <h2 class="heading"> <?php echo $heading; ?></h2>
                 <p class="preamble"> <?php echo $preamble; ?></p>
                 <div class="image_part">
-                    <a class="link_url" href="<?php echo $link_url;?>" target="_blank" rel="noreferrer noopener">
+                    <a class="link_url" href="<?php echo $link_url; ?>" target="_blank" rel="noreferrer noopener"
+                        aria-label="Boozang TheLab">
                         <img src="<?php echo $image[0]; ?>" alt="<?php echo $alt_text; ?>" />
                         <span class="alt_caption"><?php echo $alt_text; ?></span>
                     </a>
--- templates/feature_blocks.php
+++ templates/feature_blocks.php
@@ -5,62 +5,62 @@ $bg_color = get_sub_field('background_color');
 <section class="feature_blocks <?php echo $bg_color; ?> section_spacing_top_medium">
     <div class="container">
         <?php
-   $heading = get_sub_field('heading');
-if ($heading) {?>
+        $heading = get_sub_field('heading');
+        if ($heading) { ?>
         <h2 class="main_heading"> <?php echo $heading; ?></h2>
         <?php
-}?>
+        } ?>
     </div>
     <div class="container">
         <?php
-// check if the repeater field has rows
-if (have_rows('feature_blocks_row')) {
-    while (have_rows('feature_blocks_row')) {
-        the_row(); ?>
+        // check if the repeater field has rows
+        if (have_rows('feature_blocks_row')) {
+            while (have_rows('feature_blocks_row')) {
+                the_row(); ?>
 
         <div class="row align-items-start">
             <!-- <div class="row no-gutters align-items-start"> -->
             <?php
-// check if the repeater field has rows
-        if (have_rows('content')) {
-            while (have_rows('content')) {
-                the_row();
+                    // check if the repeater field has rows
+                    if (have_rows('content')) {
+                        while (have_rows('content')) {
+                            the_row();
 
-                $style = '';
-                $color = get_sub_field('color_theme');
-                $bg_image = get_sub_field('background_image');
-                $link_url = get_sub_field('link_url');
-                $overlay_color = '';
-                
-                if ($bg_image) {
-                    $style = 'style="background:url(\'' . wp_get_attachment_url($bg_image, 'full') . '\') no-repeat center; background-size: cover"';
-                    $overlay_color = get_sub_field('overlay_color');
-                }
-                $col_class = 'col-12 col-lg-4';
-                if (get_sub_field('columns_wide') == 'large') {
-                    $col_class = 'col-12 col-lg-8';
-                } ?>
+                            $style = '';
+                            $color = get_sub_field('color_theme');
+                            $bg_image = get_sub_field('background_image');
+                            $link_url = get_sub_field('link_url');
+                            $overlay_color = '';
+
+                            if ($bg_image) {
+                                $style = 'style="background:url(\'' . wp_get_attachment_url($bg_image, 'full') . '\') no-repeat center; background-size: cover"';
+                                $overlay_color = get_sub_field('overlay_color');
+                            }
+                            $col_class = 'col-12 col-lg-4';
+                            if (get_sub_field('columns_wide') == 'large') {
+                                $col_class = 'col-12 col-lg-8';
+                            } ?>
 
             <div class="<?php echo $col_class; ?>">
                 <section class="single_block <?php echo $color; ?> <?php echo $overlay_color; ?>" <?php echo $style; ?>>
                     <div class="inner_items">
-                        <h4 class=" heading"><?php the_sub_field('heading'); ?></h4>
+                        <h4 class="heading"><?php the_sub_field('heading'); ?></h4>
                         <p class="text"><?php the_sub_field('text'); ?></p>
                     </div>
-                    <?php if ($link_url) {?>
-                    <a href="<?php echo $link_url; ?>" class="link"></a>
+                    <?php if ($link_url) { ?>
+                    <a href="<?php echo $link_url; ?>" class="link" aria-label="<?php the_sub_field('heading'); ?>"></a>
                     <?php
-                        } ?>
+                                    } ?>
 
                 </section>
             </div>
             <?php
-            }
-        } ?>
+                        }
+                    } ?>
         </div>
         <?php
-    }
-}
-?>
+            }
+        }
+        ?>
     </div>
 </section>
