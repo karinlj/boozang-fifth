0161d4c4a7710e1b8770b720d24612cc069b829e
Karin Ljunggren <kaljunggren@gmail.com>
2025-04-24 23:15:43 +0200

    2025 update mobile menu and filter margin fix

--- template-parts/blog/blog-sidebar.php
+++ template-parts/blog/blog-sidebar.php
@@ -1,6 +1,6 @@
   <!--listing of categories on blog-->
 
-  <div class="blog_sidebar">
+  <div class="blog_sidebar section_spacing_top_mini">
       <div class="container">
           <?php if (is_active_sidebar('sidebar-blog')) : ?>
               <?php dynamic_sidebar('sidebar-blog'); ?>
